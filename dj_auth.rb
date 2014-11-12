8208aa19c9d33fedc6737da781f400816c79d4ab
Tony Miller <mcfiredrill@gmail.com>
2014-11-12 08:43:26 -0800

    liquidsoap script

---
+++ dj_auth.rb
@@ -0,0 +1,18 @@
+#!/usr/bin/env ruby
+
+Bundler.require
+
+require 'httparty'
+
+username = ARGV[0]
+password = ARGV[1]
+radio_name = ARGV[2]
+
+opts = { body: { :user => {"login" => username, "password" => password} } }
+
+resp = HTTParty.post("http://#{radio_name}.streampusher.io/login.json", opts)
+if resp["success"] == true
+  puts true
+else
+  puts false
+end
