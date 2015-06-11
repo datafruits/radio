c862ad08ca1fe6bb744163354e2891b062dffee9
Tony Miller <mcfiredrill@gmail.com>
2015-06-11 22:26:28 +0900

    fallback to normal key

--- liquidsoap/next_song.rb
+++ liquidsoap/next_song.rb
@@ -4,12 +4,18 @@ require_relative "./time_parser"
 
 redis = Redis.new host: ENV['REDIS_PORT_6379_TCP_ADDR'], port: ENV['REDIS_PORT_6379_TCP_PORT']
 
-key = "#{ENV['RADIO_NAME']}:playlist"
+fallback_key = "#{ENV['RADIO_NAME']}:playlist"
 
 schedule_key = TimeParser.get_key_for_current_time Time.now.utc
 playlist_key = redis.get schedule_key
 
-song = redis.rpop playlist_key
-redis.lpush playlist_key, song
+if playlist_key.nil?
+  key = fallback_key
+else
+  key = playlist_key
+end
+
+song = redis.rpop key
+redis.lpush key, song
 
 puts "/home/liquidsoap/tracks/#{song}"
