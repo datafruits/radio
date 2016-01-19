0f5382ea7f1ab15a197114d9485673c2a289c56b
Tony Miller <mcfiredrill@gmail.com>
2016-01-20 00:57:46 +0900

    prepare to use next track api

--- liquidsoap/next_song.rb
+++ liquidsoap/next_song.rb
@@ -1,21 +1,13 @@
 #!/usr/bin/env ruby
 Bundler.require
-require_relative "./time_parser"
-
-redis = Redis.new host: ENV['REDIS_PORT_6379_TCP_ADDR'], port: ENV['REDIS_PORT_6379_TCP_PORT']
-
-fallback_key = redis.get "#{ENV['RADIO_NAME']}:default_playlist"
-
-schedule_key = TimeParser.get_key_for_current_time Time.now.utc
-playlist_key = redis.get schedule_key
-
-if playlist_key.nil?
-  key = fallback_key
+if ENV['RAILS_ENV'] == 'development'
+  # TODO find a better way to get the docker host
+  host = 'docker:3000'
+  puts true
+  exit(0)
 else
-  key = playlist_key
+  host = "#{ENV['RADIO_NAME']}.streampusher.com"
 end
 
-song = redis.rpop key
-redis.lpush key, song
-
-puts "/home/liquidsoap/tracks/#{song}"
+resp = HTTPartyget.get("http://#{host}/next.json")
+JSON.parse(resp.body)
