7338789a0d8374dc6cf67a896466f4cf3be98ebe
Tony Miller <mcfiredrill@gmail.com>
2016-01-20 09:53:44 +0900

    fix next_song.rb

--- liquidsoap/next_song.rb
+++ liquidsoap/next_song.rb
@@ -1,13 +1,10 @@
 #!/usr/bin/env ruby
 Bundler.require
 if ENV['RAILS_ENV'] == 'development'
-  # TODO find a better way to get the docker host
-  host = 'docker:3000'
-  puts true
-  exit(0)
+  host = "#{ENV['RADIO_NAME']}.streampusher.com:3000"
 else
   host = "#{ENV['RADIO_NAME']}.streampusher.com"
 end
 
-resp = HTTPartyget.get("http://#{host}/next.json")
-JSON.parse(resp.body)
+resp = HTTParty.get("http://#{host}/next.json")
+puts JSON.parse(resp.body).to_json
