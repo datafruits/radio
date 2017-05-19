a0e699b49a2d10867e58da2a4ccfb1c8e14ed99c
Tony Miller <mcfiredrill@gmail.com>
2017-05-19 18:31:22 +0900

    set protocol in next_song

--- liquidsoap/next_song.rb
+++ liquidsoap/next_song.rb
@@ -2,9 +2,11 @@
 Bundler.require
 if ENV['RAILS_ENV'] == 'development'
   host = "rails:3000/radios/#{ENV['RADIO_NAME']}"
+  protocol = "http"
 else
   host = "#{ENV['RADIO_NAME']}.streampusher.com"
+  protocol = "https"
 end
 
-resp = HTTParty.get("https://#{host}/next.json")
+resp = HTTParty.get("#{protocol}://#{host}/next.json")
 puts JSON.parse(resp.body).to_json
