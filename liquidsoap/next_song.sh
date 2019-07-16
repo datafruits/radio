0d9a2277a028316eda70721ac37fb57a6a90f11b
Tony Miller <mcfiredrill@gmail.com>
2019-07-16 10:34:13 +0900

    WIP comment out stuff to get it working...

--- liquidsoap/next_song.sh
+++ liquidsoap/next_song.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 HOST=""
 
-if [ "$RAILS_ENV" == "development" ]; then
-  HOST="http://rails:3000/radios/${RADIO_NAME}/next.json"
-else
-  HOST="https://${RADIO_NAME}.streampusher.com/next.json"
-fi
+#if [ "$RAILS_ENV" == "development" ]; then
+#  HOST="http://rails:3000/radios/${RADIO_NAME}/next.json"
+#else
+#  HOST="https://datafruits.streampusher.com/next.json"
+#fi
 
-result=$(curl  -s -H "Accept: application/json" -H "Content-type: application/json" $HOST)
+#result=$(curl  -s -H "Accept: application/json" -H "Content-type: application/json" $HOST)
 
-echo $result | jq -c -r -M .
+#echo $result | jq -c -r -M .
+
+echo '{"cue_out":"0","cue_in":"0","fade_out":"0","fade_in":"0","track":"https://streampusher.s3.amazonaws.com/datafruits/l_at_dhl_05022019.mp3"}'
